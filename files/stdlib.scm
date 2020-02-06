289bd236420627bfa7f26e474bc5de9ea0b18c4c
Thomas Dost@db <Unaimend@gmail.com>
2020-02-06 15:32:25 +0100

    Added foldl

--- files/stdlib.scm
+++ files/stdlib.scm
@@ -1,6 +1,5 @@
 (define (not x)            (if x #f #t))
 (define (null? obj)        (if (eqv? obj '()) #t #f))
-(define ptere "hallo")
 
 (define head car)
 (define tail cdr)
@@ -8,3 +7,8 @@
 (define (zero? x) (= x 0))
 (define (positive? x) (>= x 0))
 (define (negative? x) (< x 0))
+
+(define (foldl func accum lst)
+  (if (null? lst)
+      accum
+      (foldl func (func accum (head lst)) (tail lst))))
