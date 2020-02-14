130f77513ed49747d14da8c87736b23cc18e9027
Thomas Dost <dostthomas@icloud.com>
2020-02-14 21:59:31 +0100

    ..

--- src/Number.hs
+++ src/Number.hs
@@ -44,6 +44,7 @@ instance Fractional LispNumber where
   (Rational a) / (Rational b) = (Rational $ a / b)
 
   fromRational i = Rational i
+
 instance Ord LispNumber where
   compare (Integer a) (Integer b) = (compare a b)
   compare (Real a) (Real b) = (compare a b)
